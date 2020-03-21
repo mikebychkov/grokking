8c85af5d947d2fe86bfba16f17427a60f37aa362
MikeBychkov <MihailBichkov@yandex.ru>
2020-03-21 13:40:17 +0500

    20200321 #1

--- src/ch01/BinarySearch.java
+++ src/ch01/BinarySearch.java
@@ -1,4 +1,22 @@
 package ch01;
 
 public class BinarySearch {
+    public static int search(int[] list, int item) {
+        int rsl = -1;
+        int low = 0;
+        int high = list.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int guess = list[mid];
+            if (guess == item) {
+                rsl = mid;
+                break;
+            } else if (guess < item) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return rsl;
+    }
 }
--- src/ch01/BinarySearchTest.java
+++ src/ch01/BinarySearchTest.java
@@ -1,4 +1,48 @@
 package ch01;
 
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/*
+    @Test
+    public void when2on2() {
+        Matrix matrix = new Matrix();
+        int[][] table = matrix.multiple(2);
+        int[][] expect = {
+                {1, 2},
+                {2, 4}
+        };
+        assertThat(table, is(expect));
+    }
+*/
+
 public class BinarySearchTest {
+    @Test
+    public void search_odd_true() {
+        int[] input = {1, 3, 5, 7, 9};
+        int result = BinarySearch.search(input, 3);
+        assertThat(result, is(1));
+    }
+
+    @Test
+    public void search_odd_false() {
+        int[] input = {1, 3, 5, 7, 9};
+        int result = BinarySearch.search(input, 10);
+        assertThat(result, is(-1));
+    }
+
+    @Test
+    public void search_even_true() {
+        int[] input = {1, 3, 5, 7, 9, 11};
+        int result = BinarySearch.search(input, 3);
+        assertThat(result, is(1));
+    }
+
+    @Test
+    public void search_even_false() {
+        int[] input = {1, 3, 5, 7, 9, 11};
+        int result = BinarySearch.search(input, 10);
+        assertThat(result, is(-1));
+    }
 }
