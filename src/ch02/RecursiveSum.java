23910fb34785006231023f10e44144411c24b9c3
Admin <mihailbichkov@yandex.ru>
2020-05-06 00:08:53 +0500

    #202005060008

--- src/ch02/RecursiveSum.java
+++ src/ch02/RecursiveSum.java
@@ -7,8 +7,23 @@ public class RecursiveSum {
         return arr[index++] + rSum(arr, index);
     }
 
-    public static void main(String[] args) {
-        int[] arr = {1, 2 , 3 , 4 , 5};
-        System.out.println(rSum(arr, 0));
+    public static int rMax(int[] arr, int index) {
+        if (arr.length == index) return 0;
+        int nextVal = rMax(arr, index + 1);
+        return arr[index] > nextVal ? arr[index] : nextVal;
+    }
+
+    public static int rSearch(int item, int[] arr, int low, int high) {
+        if (low > high) return -1;
+        int mid = (low + high) >> 1;
+        int guess = arr[mid];
+        if (guess == item) {
+            return mid;
+        } else if (guess < item) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+        return rSearch(item, arr, low, high);
     }
 }
---
+++ src/ch02/RecursiveSumTest.java
@@ -0,0 +1,49 @@
+package ch02;
+
+import ch01.BinarySearch;
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class RecursiveSumTest {
+
+    @Test
+    public void rSum() {
+        int[] arr = {1, 2 , 3 , 4 , 5};
+        assertThat(RecursiveSum.rSum(arr, 0), is(15));
+    }
+
+    @Test
+    public void rMax() {
+        int[] arr = {1, 23 , 13 , 444 , 51};
+        assertThat(RecursiveSum.rMax(arr, 0), is(444));
+    }
+
+    @Test
+    public void search_odd_true() {
+        int[] input = {1, 3, 5, 7, 9};
+        int result = RecursiveSum.rSearch(3, input, 0, input.length - 1);
+        assertThat(result, is(1));
+    }
+
+    @Test
+    public void search_odd_false() {
+        int[] input = {1, 3, 5, 7, 9};
+        int result = RecursiveSum.rSearch(10, input, 0, input.length - 1);
+        assertThat(result, is(-1));
+    }
+
+    @Test
+    public void search_even_true() {
+        int[] input = {1, 3, 5, 7, 9, 11};
+        int result = RecursiveSum.rSearch(3, input, 0, input.length - 1);
+        assertThat(result, is(1));
+    }
+
+    @Test
+    public void search_even_false() {
+        int[] input = {1, 3, 5, 7, 9, 11};
+        int result = RecursiveSum.rSearch(10, input, 0, input.length - 1);
+        assertThat(result, is(-1));
+    }
+}
